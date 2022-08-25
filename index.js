3056663bd7262ae025a06a24c706b71d15091d89
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-26 01:01:32 +0300

    Done with asiignment

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
+function isPalindrome(word) {
+  
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
+}
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return tru
 */
 
 /*
@@ -20,6 +32,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
 }
 
 module.exports = isPalindrome;
